59a07602d31816361e68f1186d07a9246356365d
sheshadris <sheshadrisharma1996@gmail.com>
2018-08-31 11:52:19 +0530

    Second Commit

---
+++ JSONImageFromURL/app/src/main/java/com/example/sys2/jsonimagefromurl/DataShowWithURL.java
@@ -0,0 +1,34 @@
+package com.example.sys2.jsonimagefromurl;
+
+public class DataShowWithURL
+              {
+                  private String Rank;
+                  private String Population;
+                  private String Flag;
+                  public String getRank() {
+                      return Rank;
+                  }
+
+                  public void setRank(String rank) {
+                      Rank = rank;
+                  }
+
+                  public String getPopulation() {
+                      return Population;
+                  }
+
+                  public void setPopulation(String population) {
+                      Population = population;
+                  }
+
+                  public String getFlag() {
+                      return Flag;
+                  }
+
+                  public void setFlag(String flag) {
+                      Flag = flag;
+
+                  }
+
+
+}
---
+++ JSONImageFromURL/app/src/main/java/com/example/sys2/jsonimagefromurl/JSONImageCustomAdapter.java
@@ -0,0 +1,111 @@
+package com.example.sys2.jsonimagefromurl;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class JSONImageCustomAdapter extends BaseAdapter {
+   // ProgressDialog progressDialog;
+
+    Context context;
+    List<DataShowWithURL> dataShowWithURLS;
+    ImageView imageView;
+    TextView textView,population;
+    public JSONImageCustomAdapter(Context context, List<DataShowWithURL> arrayList) {
+        this.context = context;
+        this.dataShowWithURLS = arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return dataShowWithURLS.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return dataShowWithURLS.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View view1 ;
+        view = LayoutInflater.from(context).inflate(R.layout.show_data_with_image,viewGroup,false);
+        view1 = view;
+       // DataShowWithURL dataShowWithURL = new DataShowWithURL();
+        imageView = view.findViewById(R.id.imageView);
+        textView = view.findViewById(R.id.name);
+        population = view.findViewById(R.id.popu);
+        textView.setText(dataShowWithURLS.get(i).getRank());
+        population.setText(dataShowWithURLS.get(i).getPopulation());
+        String url = dataShowWithURLS.get(i).getFlag();
+        Picasso.with(context).load(url).into(imageView);
+      //  new DownloadPage(imageView).execute(url);
+        return view1;
+    }
+    private class DownloadPage extends AsyncTask<String,Void,Bitmap>
+    {
+        ImageView imageView;
+        public DownloadPage(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+//            progressDialog = new ProgressDialog(context);
+//            progressDialog.setTitle("Download Image Know");
+//            progressDialog.setMessage("Loading...");
+//            progressDialog.setIndeterminate(false);
+//            progressDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String imageURL = strings[0];
+
+            Bitmap bitmap = null;
+            try {
+                //   Download Image from URL
+
+                InputStream input = new URL(imageURL).openStream();
+                // Decode Bitmap
+                bitmap = BitmapFactory.decodeStream(input);
+                input.close();
+                return bitmap;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imageView.setImageBitmap(bitmap);
+//            progressDialog.dismiss();
+        }
+    }
+}
